b90a5ec17021b04cda71b89f75430d362a4c7fed
Sean Fairchild <sfairchild@nexient.com>
2017-04-07 16:11:21 -0400

    Initial commit

---
+++ lib/spotify/rubmote.rb
@@ -0,0 +1,7 @@
+require "spotify/rubmote/version"
+
+module Spotify
+  module Rubmote
+    # Your code goes here...
+  end
+end
---
+++ lib/spotify/rubmote/version.rb
@@ -0,0 +1,5 @@
+module Spotify
+  module Rubmote
+    VERSION = "0.1.0"
+  end
+end
