e9309f784482f963be63262b849f75dfd2a8576c
Sean Fairchild <sfairchild@nexient.com>
2017-04-11 12:31:45 -0400

    Get a csrf token

--- lib/rubmote/spotify.rb
+++ lib/rubmote/spotify.rb
@@ -8,12 +8,21 @@ module Rubmote
 		base_uri 'https://open.spotify.com/'
 
 		def initialize
-			@token = get_token
+			@token			= get_token
+			@csrf_token = get_csrf_token
 		end
 
 		private
 		def get_token
 			JSON.parse(self.class.get('/token').body)['t']
+		end
+
+		def get_csrf_token
+			JSON.parse(self.class.get("https://#{ random_localhost }.spotilocal.com:4370/simplecsrf/token.json").body)['token']
+		end
+
+		def random_localhost
+			10.times.map { ([*'a'..'z']).sample }.join
 		end
 	end
 end
--- spec/rubmote/spotify_spec.rb
+++ spec/rubmote/spotify_spec.rb
@@ -3,12 +3,17 @@ require 'spec_helper'
 describe Rubmote::Spotify do
 	before(:each) do
 		stub_request(:get, "https://open.spotify.com/token").to_return(token_success)
+		stub_request(:get, /[a-z]{10}\.spotilocal\.com\:4370\/simplecsrf\/token\.json/).to_return(csrf_token_success)
 	end
 
 	subject { Rubmote::Spotify.new }
 
 	let(:token_success) {
 		File.new(File.join(RSpec.root, 'sample_responses', 'open.spotify.com', 'token', 'success.txt'))
+	}
+
+	let(:csrf_token_success) {
+		File.new(File.join(RSpec.root, 'sample_responses', 'spotilocal.com', 'token.json', 'success.txt'))
 	}
 
   it 'has a token reader method' do
@@ -27,10 +32,19 @@ describe Rubmote::Spotify do
     expect(Rubmote::Spotify.method_defined?(:'csrf_token=')).to be false
   end
 
-	describe 'a new Spotify Instance' do
-
+	it 'gets an random localhost' do
+		random_host = subject.send(:random_localhost)
+		expect(random_host).to match(/[a-z]{10}/)
 	end
+
+  describe '#initialize' do
+
     it 'gets a new token' do
       expect(subject.token).to eq('NAowChgKB1Nwb3RpZnkSABoGmAEByAEBJV1W7VgSFPLkH2iipjle98GYNEiBSFE-9Ymq')
     end
+
+    it 'gets a new csrf token' do
+      expect(subject.csrf_token).to eq('f18505af00afa8d4014a9fef3a4c6a9f')
+    end
+  end
 end
