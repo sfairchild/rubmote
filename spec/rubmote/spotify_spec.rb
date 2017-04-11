f3f3217d594a1ba3d1b27a0e9c7a51a741bae24a
Sean Fairchild <sfairchild@nexient.com>
2017-04-11 12:31:24 -0400

    Test getting a new token

--- spec/rubmote/spotify_spec.rb
+++ spec/rubmote/spotify_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe Rubmote::Spotify do
+	before(:each) do
+		stub_request(:get, "https://open.spotify.com/token").to_return(token_success)
+	end
+
+	subject { Rubmote::Spotify.new }
+
+	let(:token_success) {
+		File.new(File.join(RSpec.root, 'sample_responses', 'open.spotify.com', 'token', 'success.txt'))
+	}
+
   it 'has a token reader method' do
     expect(Rubmote::Spotify.method_defined?(:token)).to be true
   end
@@ -20,4 +30,7 @@ describe Rubmote::Spotify do
 	describe 'a new Spotify Instance' do
 
 	end
+    it 'gets a new token' do
+      expect(subject.token).to eq('NAowChgKB1Nwb3RpZnkSABoGmAEByAEBJV1W7VgSFPLkH2iipjle98GYNEiBSFE-9Ymq')
+    end
 end
